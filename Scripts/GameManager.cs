ce9ff22570cd5083ad8ff90668184b0615399076
pbfrias17 <pbfrias17@gmail.com>
2015-08-31 09:52:19 -0400

    woot

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameManager {
+
+	public static bool miniGameOn;
+
+	// Use this for initialization
+	static void Start () {
+	
+	}
+
+	public static void StartMiniGame() {
+		miniGameOn = true;
+	}
+
+	// Update is called once per frame
+	static void Update () {
+	
+	}
+}
--- Scripts/GiftGuessing.cs
+++ Scripts/GiftGuessing.cs
@@ -8,27 +8,54 @@ public class GiftGuessing : MonoBehaviour {
 	private List<string> giftStrings = new List<string>();
 	private GameObject[] giftObjects;
 	private bool gameStart;
-	public Sprite[] giftSprites;
+	private Sprite[] giftSprites;
 	private string giftType;
-	
+	private int giftAmt;
+	private int correctGiftIndex;
 
 	// Use this for initialization
-	void Start () {
-		gameStart = true;
-		giftStrings.Add("doll");
-		giftType = "doll";
-		giftObjects = GameObject.FindGameObjectsWithTag("GiftGuess");
-		foreach(GameObject gift in giftObjects) {
-			Debug.Log("Found " + gift);
+	void Start() {
+
+		//load all sprites, randomly pick a correct one
+		//show it to the user
+		giftSprites = Resources.LoadAll<Sprite>("Sprites/GiftGuessing/" + giftType);
+		correctGiftIndex = 0;
+
+		//handle all GameObject creation via scripts
+		GameObject correctGift = new GameObject();
+
+		//handle MiniGame
+		GameManager.StartMiniGame();
+		Debug.Log ("GM: " + GameManager.miniGameOn);
+
+
+		if (GameManager.miniGameOn) {
+			//initialize gifts and sprites
+			giftAmt = 3;
+			giftObjects = new GameObject[giftAmt];
+			for (int i = 0; i < giftAmt; i++) {
+				GameObject gift = new GameObject();
+				gift.AddComponent<SpriteRenderer>();
+				gift.tag = "GiftGuess";
+				giftObjects [i] = gift;
+			}
+			LoadGifts();
 		}
-		giftSprites = Resources.LoadAll<Sprite>("Sprites/GiftGuessing/" + giftType);  
-		AssignSprites();
 	}
 
-	void AssignSprites() {
+	void LoadGifts() {
+		float xPos = -3.5f;
+		float xGap = 3.5f;
+		float yPos = 0f;
+		float zPos = 0f;
+
 		foreach(GameObject gift in giftObjects) {
 			SpriteRenderer SR = gift.GetComponent<SpriteRenderer>();
+			Transform T = gift.GetComponent<Transform>();
+			T.transform.position = new Vector3(xPos, yPos, zPos);
+			T.transform.localScale = new Vector3(2.5f, 2.5f, 1);
 			SR.sprite = giftSprites[0];
+			xPos += xGap;
 		}
 	}
 	
