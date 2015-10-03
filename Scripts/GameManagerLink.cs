e2d954cf038d28b17b7b840671071907b8dbd452
pbfrias17 <pbfrias17@gmail.com>
2015-10-03 12:19:01 -0400

    Revert "its all ruined"
    This reverts commit 2be0d953311076c3af7df63225a1582b3bea0a09.

---
+++ Scripts/GameManagerLink.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerLink : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		GameManager.Init();
+	}
+
+	public void Init() {
+		Canvas UI = (Canvas) FindObjectOfType(typeof(Canvas));
+
+		//hide main UI and start minigames
+		UI.gameObject.SetActive(false);
+		GameManager.GameLoop();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Scripts/Util.cs
+++ Scripts/Util.cs
@@ -3,8 +3,20 @@ using System.Collections;
 
 public static class Util {
 
-	public static GameObject CreateButton() {
-		return new GameObject();
+	public static void ApplySpriteToObject(ref GameObject obj, Sprite sp) {
+		//properly fits sprite to object 
+		//taking screen size into consideration
+		SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+		if (sr == null) return;
+		sr.sprite = sp;
+		
+		obj.transform.localScale = new Vector3(1f,1f,1f);
+		
+		float width = sr.sprite.bounds.size.x;
+		float height = sr.sprite.bounds.size.y;
+		double worldScreenHeight = Camera.main.orthographicSize * 2.0;
+		double worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+		
+		obj.transform.localScale = new Vector2((float)worldScreenWidth / width, (float)worldScreenHeight / height);
 	}
-
 }
